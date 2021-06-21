329c24c21ebc2f3ae0d78439a1a1a856671c0f23
sczx2328 <75553583+sczx2328@users.noreply.github.com>
2021-06-21 22:35:07 +0800

    Create Main.scala

---
+++ 大数据作业3/实现项目/S3_Flink/源文件/Main.scala
@@ -0,0 +1,65 @@
+//通过S3上传文件然后Kafka读取S3指定桶中数据存入到指定主题中
+import java.util.Properties
+import com.bingocloud.{ClientConfiguration, Protocol}
+import com.bingocloud.auth.BasicAWSCredentials
+import com.bingocloud.services.s3.AmazonS3Client
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.nlpcn.commons.lang.util.IOUtil
+
+object Main {
+  //s3参数
+  val accessKey = "1603961BCC1DC7F1F503"
+  val secretKey = "WzA3OTVGQzQ5RkJDQzY4QUI2MjA4RkI5OUYyMEUx"
+  val endpoint = "http://scut.depts.bingosoft.net:29997"
+  val bucket = "john"
+  //要读取的文件
+  val key = "peace.txt"
+
+  //kafka参数
+  val topic = "mn_buy_ticket_P&L"
+  val bootstrapServers = "bigdata35.depts.bingosoft.net:29035,bigdata36.depts.bingosoft.net:29036,bigdata37.depts.bingosoft.net:29037"
+
+  def main(args: Array[String]): Unit = {
+    val s3Content = readFile()
+    produceToKafka(s3Content)
+  }
+
+  /**
+   * 从s3中读取文件内容
+   *
+   * @return s3的文件内容
+   */
+  def readFile(): String = {
+    val credentials = new BasicAWSCredentials(accessKey, secretKey)
+    val clientConfig = new ClientConfiguration()
+    clientConfig.setProtocol(Protocol.HTTP)
+    val amazonS3 = new AmazonS3Client(credentials, clientConfig)
+    amazonS3.setEndpoint(endpoint)
+    val s3Object = amazonS3.getObject(bucket, key)
+    IOUtil.getContent(s3Object.getObjectContent, "UTF-8")
+  }
+
+  /**
+   * 把数据写入到kafka中
+   *
+   * @param s3Content 要写入的内容
+   */
+  def produceToKafka(s3Content: String): Unit = {
+    val props = new Properties
+    props.put("bootstrap.servers", bootstrapServers)
+    props.put("acks", "all")
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
+    val producer = new KafkaProducer[String, String](props)
+    val dataArr = s3Content.split("\n")
+    for (s <- dataArr) {
+      if (!s.trim.isEmpty) {
+        val record = new ProducerRecord[String, String](topic, null, s)
+        println("开始生产数据：" + s)
+        producer.send(record)
+      }
+    }
+    producer.flush()
+    producer.close()
+  }
+}
